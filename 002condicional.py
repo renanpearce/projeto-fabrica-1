1d13709e4c6ad8244c345bba8c4176c8cb222418
renanpearce <pearce2renan@gmail.com>
2025-09-10 14:03:10 -0300

    Add files via upload

--- 002condicional.py
+++ 002condicional.py
@@ -1,28 +1,31 @@
-# Crie um código que receba 3 notas, calcule a média 
-# e informe se o aluno está aprovado, em recuperação 
-# ou reprovado
+# crie um código que receba 3 notas, calcule a media 
+# e informe se o aluno esta aprovado, em recuperação
+# ou reprovado 
+# etapas
+# OBS: aprovado >= 7
+#recuperação media > 4 
+#reprovado < 4
 
-# OBS: Aprovado media >= 7
-# Recuperação média > 4
-# Reprovado média < 4
 
-# Etapas 
-# 1) Solicitar as notas ao usuário
-primeira_nota = float(input("Digite a primeira nota: "))
-segunda_nota = float(input("Digite a segunda nota: "))
-terceira_nota = float(input("Digite a terceira nota: "))
+#  1) solicitar as notas ao usuario 
+# 2) calcular media
+# 3) checar a condiçao do aluno 
+# 4) infomar os resultados 
 
-# 2) Calcular a média
-media = (primeira_nota + segunda_nota + terceira_nota)/3
+#etapas
+# 1) solicitar  as notas ao usuario 
+n1=float(input("digite a primeira nota:"  ))
+n2 = float(input ("digite a n2: "))
+n3  = float(input("digite a n3:"))
 
-# 3) Checar a condição do aluno
-# 4) Informar o resultado
-if media >= 7:
-    print(f"O aluno tem média {media:.2f} e está aprovado.")
-elif 5 <= media < 7: #  o mesmo que  => elif media >= 5 and media < 7:
-    print(f"O aluno teve média {media:.2f} e está em recuperação.")
-    print(f"O aluno teve média {media:.2f} e está em recuperação.")
+# 2)calcular media
+media = (n1 + n2 + n3)/3
+
+# 3) checar a condição do aluno 
+if media >=7:
+    print(f"o aluno tem media {media:.2f} e esta aprovado. ")
+elif media > 4:
+    print(f"o aluno teve media {media: . 2f} e esta de uperação.")
+
 else:
-    print(f"O aluno teve média {media:.2f} e está reprovado.")
-
-
+    print(f"o aluno teve media {media:.2f} e esta reprovado")
